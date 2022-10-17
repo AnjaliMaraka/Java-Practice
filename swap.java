f44ff1dcaff895bb3aba8cdab5282f2e62acff56
AnjaliMaraka <114507302+AnjaliMaraka@users.noreply.github.com>
2022-10-17 18:30:50 +0530

    Add files via upload

---
+++ swap.java
@@ -0,0 +1,12 @@
+class swap {
+  public static void main(String args[]) {
+    int a, b, temp;
+    a = 2;
+    b = 3;
+    temp = a;
+    a = b;
+    b = temp;
+    System.out.println("print a: " + a);
+    System.out.println("print b: " + b);
+  }
+}
