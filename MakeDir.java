c072f1953efa15123b1fd9f75ce374c318220799
AnjaliMaraka <114507302+AnjaliMaraka@users.noreply.github.com>
2023-01-03 10:08:33 +0530

    Add files via upload

---
+++ MakeDir.java
@@ -0,0 +1,6 @@
+import java.io.*;
+class MakeDir{
+  public static void main(String args[]){
+    File file=new File()
+  }
+}
---
+++ Setpath.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+class Setpath {
+  public static void main(String args[]){
+    
+       File file=new File("Data.txt");
+    if(file.exists()){
+      System.out.println(file.getName());
+      System.out.println("absolute path"+file.getAbsolutePath());
+      System.out.println("is file writeable"+file.canWrite());
+      System.out.println("is file readable"+file.canRead());
+      System.out.println(file.length());
+      }
+    else{
+      System.out.println("the file doesnot exist");
+    }
+  }
+}
+ 
+
+
+  
